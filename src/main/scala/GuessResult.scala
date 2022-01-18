ceda287b8bc09b9658dfd546141c4276ee98fd6f
Eric Riese <eric.riese@gmail.com>
2022-01-18 10:40:45 -0500

    refactoring and List.fill

--- src/main/scala/GuessResult.scala
+++ src/main/scala/GuessResult.scala
@@ -6,18 +6,11 @@ case class GuessResult(
                         somewhere: Set[Char]
                       ) {
   def +(other: GuessResult): GuessResult = {
-    val sumOfPositionals: List[CharKnowledge] = (this.positional zip other.positional).map {
-      case (Known(c), _) => Known(c)
-      case (_, Known(c)) => Known(c)
-      case (Known(a), Known(b)) if a != b => throw new Exception(s"determined to be both $a and $b")
-      case (Not(cs1: Set[Char]), Not(cs2: Set[Char])) => Not(cs1 ++ cs2)
-      case (a, Unknown) => a
-      case (Unknown, a) => a
-    }
+    val sumOfPositionals = combinePositionals(other.positional)
     val knownLetters = sumOfPositionals.collect {
       case Known(c) => c
     }.toSet
-    val sumOfSomewhere: Set[Char] = (this.somewhere ++ other.somewhere) -- knownLetters
+    val sumOfSomewhere = (this.somewhere ++ other.somewhere) -- knownLetters
 
     GuessResult(
       sumOfPositionals,
@@ -25,10 +18,21 @@ case class GuessResult(
       sumOfSomewhere
     )
   }
+
+  private def combinePositionals(otherPositional: List[CharKnowledge]): List[CharKnowledge] = {
+    (this.positional zip otherPositional).map {
+      case (Known(a), Known(b)) if a != b => throw new Exception(s"determined to be both $a and $b")
+      case (Known(c), _) => Known(c)
+      case (_, Known(c)) => Known(c)
+      case (Not(cs1: Set[Char]), Not(cs2: Set[Char])) => Not(cs1 ++ cs2)
+      case (a, Unknown) => a
+      case (Unknown, a) => a
+    }
+  }
 }
 
 object KnowNothing extends GuessResult(
-  Array.fill(5)(Unknown).toList,
+  List.fill(5)(Unknown),
   Set(),
   Set()
 )
@@ -45,31 +49,31 @@ object GuessResult {
    */
   def parse(play: String, response: String): GuessResult = {
     response.ensuring(_.length == 5, "Response must be 5 characters long")
-    (play zip response).zipWithIndex.collect {
+    (play zip response).zipWithIndex.map {
       case ((c, 'v'), i) => knowOneLetterAt(c, i)
       case ((c, 'x'), _) => no(c)
       case ((c, '~'), i) => elsewhere(c, i)
-      case _ =>  throw new Exception("Invalid response format")
+      case _ => throw new Exception("Invalid response format")
     }.reduce(_ + _)
   }
 
   private def knowOneLetterAt(c: Char, i: Int): GuessResult =
     GuessResult(
-      Array.fill(5)(Unknown).toList.updated(i, Known(c)),
+      List.fill(5)(Unknown).updated(i, Known(c)),
       Set(),
       Set()
     )
 
   private def no(c: Char): GuessResult =
     GuessResult(
-      Array.fill(5)(Unknown).toList,
+      List.fill(5)(Unknown),
       Set(c),
       Set()
     )
 
   def elsewhere(c: Char, i: Int): GuessResult =
     GuessResult(
-      Array.fill(5)(Unknown).toList.updated(i, Not(Set(c))),
+      List.fill(5)(Unknown).updated(i, Not(Set(c))),
       Set(),
       Set(c)
     )
